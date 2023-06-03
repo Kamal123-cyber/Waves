070fe83ce87a5c7f903e5b92c9d02e78dbd5014b
Kamal Maurya <68643049+Kamal123-cyber@users.noreply.github.com>
2023-06-03 12:47:14 +0530

    Update settings.py

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -11,11 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-import environ
-
-env = environ.Env()
-# reading .env file
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -151,8 +146,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_ACCESS_KEY_ID = 'AKIA5CYOZC3356OVZ6PU'
+AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'chitchatbucket'
 
 AWS_S3_FILE_OVERWRITE = False
